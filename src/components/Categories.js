8e2203805a708bfacbc98d58bab8dc3dd24449cd
John H <johnnyhill13@gmail.com>
2019-11-05 12:26:04 -0800

    maps categories JSON creating Category Card for each

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,50 +1,81 @@
 import React, { Component, Card, Img } from "react";
 
-import Title from './Title';
+import Title from "./Title";
 
+const DATA = [
+  {
+    id: "1",
+    name: "Clothes"
+  },
+  {
+    id: "2",
+    name: "Technology"
+  },
+  {
+    id: "3",
+    name: "Food"
+  },
+  {
+    id: "4",
+    name: "Outdoors"
+  },
+  {
+    id: "5",
+    name: "Cosmetics"
+  },
+  {
+    id: "6",
+    name: "Accessories"
+  },
+  {
+    id: "7",
+    name: "Furniture"
+  },
+  {
+    id: "8",
+    name: "Vehicles"
+  }
+];
 
 class Categories extends Component {
+  constructor() {
+    super();
+    this.state = { items: [] };
+  }
+
+  componentDidMount() {
+    fetch("")
+      .then(response => response.json())
+      .then(({ results: items }) => this.setState({ items }))
+      .catch(function(error) {
+        console.log("failed", error);
+      });
+  }
+
   render() {
+    const listCategory = DATA.map(category => (
+        <div className="col-lg-3 col-md-4 col-sm-6 my-2 text-center">
+          <div className="card bg-dark text-black">
+            <img
+              className="card-img"
+              src="https://usapears.org/wp-content/uploads/2015/08/v-bartlett.png"
+              alt="Pear"
+            />
+            <div className="card-body bg-dark text-white">
+              <h5 className="card-title">{category.name}</h5>
+            </div>
+          </div>
+        </div>
+    ));
+
+    let items = this.state.items;
+
     return (
       <React.Fragment>
-        <Title name="Categories" title="All Items"/>
-        <div className="card-deck col-10 row mx-auto my-2 text-center">
-          <div className=" card bg-dark text-black">
-            <img className="card-img" src="https://usapears.org/wp-content/uploads/2015/08/v-bartlett.png" alt="Pear" />
-            <div className="card-body bg-dark text-white">
-              <h5 className="card-title">Pears</h5>
-            </div>
-          </div>
-          <div className="card bg-dark text-black">
-            <img className="card-img" src="https://d2lnr5mha7bycj.cloudfront.net/product-image/file/primary_b81c9eb8-c227-4290-9ac5-f519de889802.jpg" alt="Oranges" />
-            <div className="card-body bg-dark text-white">
-              <h5 className="card-title">Oranges</h5>
-            </div>
-          </div>
-          <div className="card bg-dark text-white">
-            <img className="card-img" src="https://media.licdn.com/dms/image/C4E0BAQH78e3EtPWnEg/company-logo_200_200/0?e=2159024400&v=beta&t=8lr3Xm0OctUPn68LkX4iUi9IYuVIGI82v7UvyOwI4-U" alt="Apples" />
-            <div className="card-body bg-dark text-white">
-              <h5 className="card-title">Apples</h5>
-            </div>
-          </div>
-          <div className="card bg-dark text-black">
-            <img className="card-img" src="https://d2lnr5mha7bycj.cloudfront.net/product-image/file/primary_417d89ed-1022-48ac-83fb-49cc5b67b5fc.jpg" alt="Bananas" />
-            <div className="card-body bg-dark text-white">
-              <h5 className="card-title">Bananas</h5>
-            </div>
-          </div>
-          <div className="card bg-dark text-black">
-            <img className="card-img" src="https://s3.amazonaws.com/ucdim/wp-content/uploads/20180220163904/Feb_26_StrawberryDay_Body2_464456760-200x200.jpg" alt="Strawberry" />
-            <div className="card-body bg-dark text-white">
-              <h5 className="card-text">Strawberry</h5>
-            </div>
-          </div>
-          <div className="card bg-dark text-black">
-            <img className="card-img" src="https://images-na.ssl-images-amazon.com/images/I/71%2BqAJehpkL._AC_UL200_SR200,200_.jpg" alt="Pineapple" />
-            <div className="card-body bg-dark text-white">
-              <h5 className="card-text">Pineapples</h5>
-            </div>
-          </div>
+        <Title name="Categories" title="All Items" />
+        <div className="container">
+          {/* listCategory maps the JSON response and renders that number of categories */}
+          <div className="row">{listCategory}</div>
         </div>
       </React.Fragment>
     );
