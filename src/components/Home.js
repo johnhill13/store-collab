67a6548481e59af98c9fed0aae96b5e977cc58e9
pouyarezvani <32917154+pouyarezvani@users.noreply.github.com>
2019-11-05 19:14:51 -0800

    Changes Navbar and adds Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home Page</h1>
-      </div>
+      <header class="masthead">
+        <div class="container d-flex h-100 align-items-center">
+          <div class="mx-auto text-center">
+            <h1 class="mx-auto my-0 text-uppercase">Welcome to our shop</h1>
+            <h2 class="text-white-50 mx-auto mt-2 mb-5">
+              An app created with React that has everything you need!
+            </h2>
+            <Link to="/categories" class="btn btn-primary get-started-btn">
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
     );
   }
 }
