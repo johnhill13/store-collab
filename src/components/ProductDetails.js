9dd586e10d4c86976e6d31bf5cdda2526f89778b
John H <johnnyhill13@gmail.com>
2019-11-11 20:54:27 -0800

    fixes boostrap carousel issue

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Carousel from "react-bootstrap/Carousel";
+import Carousel from 'react-bootstrap/Carousel';
 
 export class ProductDetails extends Component {
   render() {
