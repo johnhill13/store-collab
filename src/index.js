4823ff42774cb72d775ec8d95aaf9045b3dfe7f0
John H <johnnyhill13@gmail.com>
2019-12-19 13:58:03 -0800

    creates reducers folder and store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import {Provider} from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+    <Provider store={store}> <App /> </Provider>,
   document.getElementById("root")
 );
 
